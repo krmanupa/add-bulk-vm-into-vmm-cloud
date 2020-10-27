6563145296b76a997d516cd0232d67be34c0cd3e
Jinesh Shah <jinsha@microsoft.com>
2020-10-27 12:47:30 +0530

    initial commit

---
+++ Add%20Bulk%20VM%20to%20Cloud.ps1
@@ -0,0 +1,24 @@
+﻿# Add Bulk VM to Cloud
+# Author Robert Smit
+# Company @ClusterMVP
+# Date April 10, 2015
+# Version 1.0
+
+
+#set cloud 
+$cloud = Get-SCCloud -Name "Tenant"
+
+# Get all VM Adjust parameter
+get-VM | Where-Object Name -Like WAPload* |ft name, selfserviceuserrole,owner
+
+#get All VM's without a Cloud
+get-VM | Where-Object Name -Like WAPload* | Where-Object {-not $_.cloud} |ft name 
+
+#place VM in Variable
+$vmtocloud=get-VM | Where-Object Name -Like WAPload* | Where-Object {-not $_.cloud} |ft name 
+#show Slected VM
+$vmtocloud |ft name 
+
+#Place VM in selected Cloud
+$vmtocloud=get-VM | Where-Object Name -Like WAPload* | Where-Object {-not $_.cloud} |Set-VM -Cloud $cloud
+
